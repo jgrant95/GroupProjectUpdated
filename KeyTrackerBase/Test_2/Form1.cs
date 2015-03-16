c425c749afe272efdafd6e1ac5ff993ef0d8d702
jgrant95 <jongrant95@gmail.com>
2015-03-16 12:03:41 +0000

    testing more
    Getting fullstop to work

--- KeyTrackerBase/Test_2/Form1.cs
+++ KeyTrackerBase/Test_2/Form1.cs
@@ -86,45 +86,81 @@ namespace KeyTrackerBase
 
             #region Key Entry Logger (Main)
             //ensures backspace isnt entered into string
+            //if (e.KeyCode != Keys.Back)
+            //{
+            //    sentence += ((char)e.KeyValue).ToString().ToLower();
+            //    textBox1.Text = sentence;
+            //    //ends sentence and logs when word count hits 30, character count hits 180, full stop is entered or the return key is entered
+            //    if (countWord == 30 || countChar == 180 || e.KeyCode == Keys.OemPeriod || e.KeyCode == Keys.Return)
+            //        {//asks, is string not empty? & is the string not have \r (what the return key produces)
+            //            if (sentence != "" && sentence != "\r")
+            //            {
+            //                using (StreamWriter file = new StreamWriter(@"C:\\Users\\computing\\Desktop\\GroupProjectUpdated\\KeyTrackerBase\\log.txt", true))
+            //                {
+            //                    //writes entries to txt file - logger
+            //                    file.WriteLine(DateTime.Now.ToString() + ":  " + sentence);
+            //                    //string and ints reset
+            //                    sentence = "";
+            //                    countWord = 0;
+            //                    countChar = 0;
+            //                    file.Close();
+            //                }
+                            
+            //            }
+            //            else
+            //                //ensures the 'returns' arent kept in txt file
+            //                sentence = "";
+
+
+
+
+
+            //        //StreamReader file = new StreamReader("C:\\Users\\Jon\\Desktop\\GroupProjectUpdated\\KeyTrackerBase\\log.txt");
+            //        //while((word = file.ReadLine()) != null)
+            //        //{
+            //        //    if (textBox1.Text == word)
+            //        //        MessageBox.Show("CUNT ALERT!");
+            //        //}
+            //        //countWord++;
+            //        //file.Close();
+            //    }
+            //}
+            #endregion
+
+            #region Try 2
+
             if (e.KeyCode != Keys.Back)
             {
                 sentence += ((char)e.KeyValue).ToString().ToLower();
                 textBox1.Text = sentence;
-                //ends sentence and logs when word count hits 30, character count hits 180, full stop is entered or the return key is entered
-                if (countWord == 30 || countChar == 180 || e.KeyCode == Keys.OemPeriod || e.KeyCode == Keys.Return)
-                    {//asks, is string not empty? & is the string not have \r (what the return key produces)
-                        if (sentence != "" && sentence != "\r")
+                if (sentence != "\r")
+                {
+                    if (sentence != "¾")
+                    {
+                        if (countWord == 30 || countChar == 180 || e.KeyCode == Keys.OemPeriod || e.KeyCode == Keys.Return)
                         {
-                            using (StreamWriter file = new StreamWriter(@"C:\\Users\\Jon\\Desktop\\GroupProjectUpdated\\KeyTrackerBase\\log.txt", true))
+                            if (sentence != "")
                             {
-                                //writes entries to txt file - logger
-                                file.WriteLine(DateTime.Now.ToString() + ":  " + sentence);
-                                //string and ints reset
-                                sentence = "";
-                                countWord = 0;
-                                countChar = 0;
-                                file.Close();
+                                using (StreamWriter file = new StreamWriter(@"C:\\Users\\computing\\Desktop\\GroupProjectUpdated\\KeyTrackerBase\\log.txt", true))
+                                {
+                                    //writes entries to txt file - logger
+                                    file.WriteLine(DateTime.Now.ToString() + ":  " + sentence);
+                                    //string and ints reset
+                                    sentence = "";
+                                    countWord = 0;
+                                    countChar = 0;
+                                    file.Close();
+                                }
                             }
-                            
                         }
-                        else
-                            //ensures the 'returns' arent kept in txt file
-                            sentence = "";
-
-
-
-
-
-                    //StreamReader file = new StreamReader("C:\\Users\\Jon\\Desktop\\GroupProjectUpdated\\KeyTrackerBase\\log.txt");
-                    //while((word = file.ReadLine()) != null)
-                    //{
-                    //    if (textBox1.Text == word)
-                    //        MessageBox.Show("CUNT ALERT!");
-                    //}
-                    //countWord++;
-                    //file.Close();
+                    }
+                    else
+                        sentence = "";
                 }
+                else
+                    sentence = "";
             }
+
             #endregion
 
         }
